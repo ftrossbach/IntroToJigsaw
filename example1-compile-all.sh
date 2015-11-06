7deaf09c3dc4de905324cfd40ccbefae81538ba4
Florian Troßbach <florian.trossbach@codecentric.de>
2015-11-06 14:06:21 +0100

    adding output echoing

--- example1-compile-all.sh
+++ example1-compile-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+set -x #echo on
 $JAVA9_BIN/javac -d example1 -modulesourcepath example1 $(find example1 -name "*.java")
--- example1-run.sh
+++ example1-run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+set -x #echo on
 $JAVA9_BIN/java -modulepath example1 -m de.codecentric.addresschecker/de.codecentric.addresschecker.api.Run $1
