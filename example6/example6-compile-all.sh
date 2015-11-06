4ffce48138ffed18fca9a01186186fe36650f8fb
Florian Troßbach <florian.trossbach@codecentric.de>
2015-11-06 19:11:50 +0100

    example 6 change in shell script (still failing to compile)

--- example6/example6-compile-all.sh
+++ example6/example6-compile-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x #echo on
-$JAVA9_BIN/javac -d . -classpath ../classpath/* -modulesourcepath .  $(find . -name "*.java")
+$JAVA9_BIN/javac -d . -classpath ../classpath -modulesourcepath .  $(find . -name "*.java")
