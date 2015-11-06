81dcd51600dcc9db42f8118e5b76e1a23954be35
Florian Troßbach <florian.trossbach@codecentric.de>
2015-11-06 14:29:06 +0100

    adding example 3

---
+++ example3/de.codecentric.addresschecker/de/codecentric/addresschecker/internal/AddressCheckerImpl.java
@@ -0,0 +1,25 @@
+package de.codecentric.addresschecker.internal;
+
+import de.codecentric.addresschecker.api.AddressChecker;
+import de.codecentric.zipvalidator.api.ZipCodeValidator;
+
+
+/**
+ * Created by ftr on 06/11/15.
+ */
+public class AddressCheckerImpl implements AddressChecker {
+    @Override
+    public boolean checkZipCode(String zipCode) {
+        ClassLoader classLoader = AddressCheckerImpl.class.getClassLoader();
+        try {
+            Class aClass = classLoader.loadClass("de.codecentric.zipvalidator.internal.ZipCodeValidatorImpl");
+            return ((ZipCodeValidator)aClass.newInstance()).zipCodeIsValid(zipCode);
+        } catch (Exception e) {
+            throw new  RuntimeException(e);
+        }
+
+
+
+
+    }
+}
