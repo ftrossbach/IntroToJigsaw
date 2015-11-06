b35f02f0252a1e0cbabb12eb78eb2cad878a6e89
Florian Troßbach <florian.trossbach@codecentric.de>
2015-11-06 20:57:48 +0100

    adding jars

--- example6/de.codecentric.zipvalidator/de/codecentric/zipvalidator/internal/ZipCodeValidatorImpl.java
+++ example6/de.codecentric.zipvalidator/de/codecentric/zipvalidator/internal/ZipCodeValidatorImpl.java
@@ -2,7 +2,6 @@ package de.codecentric.zipvalidator.internal;
 
 import de.codecentric.zipvalidator.api.ZipCodeValidator;
 import de.codecentric.zipvalidator.model.api.ZipCodeValidationResult;
-import com.google.common.base.Strings;
 
 
 /**
@@ -13,7 +12,7 @@ public class ZipCodeValidatorImpl implements ZipCodeValidator {
     @Override
     public ZipCodeValidationResult zipCodeIsValid(String zipCode) {
 
-        if (Strings.isNullOrEmpty(zipCode)) {
+        if (zipCode == null) {
             return ZipCodeValidationResult.ZIP_CODE_NULL_OR_EMPTY;
         } else if (zipCode.length() < 5) {
             return ZipCodeValidationResult.ZIP_CODE_TOO_SHORT;
--- example6/de.codecentric.zipvalidator/module-info.java
+++ example6/de.codecentric.zipvalidator/module-info.java
@@ -2,12 +2,7 @@ module de.codecentric.zipvalidator{
         exports de.codecentric.zipvalidator.api;
         requires public de.codecentric.zipvalidator.model;
 
-        //requires java.management;
-        //requires guava;
-        //requires jsr305;
 
-
-
-
+        requires commonslang3;
 
         }
