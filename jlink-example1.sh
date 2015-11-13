1073eec91881b0aa4afcbae550498a41a5870131
Florian Troßbach <florian.trossbach@codecentric.de>
2015-11-13 21:07:14 +0100

    fixing examples

--- jlink-example1.sh
+++ jlink-example1.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -x #echo on
-$JAVA9_BIN/jlink --modulepath $JAVA9_BIN/../../images/jmods/:example1/bin --addmods de.codecentric.addresschecker --output linkedjdk --exclude-files *.diz --compress-resources on --strip-java-debug on --compress-resources-level 2
+cd two-modules-ok
+./package-zipvalidator.sh
+./package-addresschecker.sh
+cd ..
+$JAVA9_BIN/jlink --modulepath $JAVA9_BIN/../../images/jmods/:two-modules-ok/bin --addmods de.codecentric.addresschecker --output linkedjdk --exclude-files *.diz --compress-resources on --strip-java-debug on --compress-resources-level 2
--- three-modules-no-implied-readability/de.codecentric.addresschecker/module-info.java
+++ three-modules-no-implied-readability/de.codecentric.addresschecker/module-info.java
@@ -1,7 +1,6 @@
 module de.codecentric.addresschecker{
         exports de.codecentric.addresschecker.api;
         requires de.codecentric.zipvalidator;
-        requires de.codecentric.zipvalidator.model;
 
 
         }
