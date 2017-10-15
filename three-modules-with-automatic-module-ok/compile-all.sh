6da99473fc7250d3e66ef1eb6e8f1ee091723fd9
Mehul Garnara <mehul.garnara@capitalone.com>
2017-10-15 16:25:14 -0400

    chore(build): change commandline argu per latest JDK9 changes to compile examples

--- three-modules-with-automatic-module-ok/compile-all.sh
+++ three-modules-with-automatic-module-ok/compile-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x #echo on
-$JAVA9_BIN/javac -d . --module-path ../jars -modulesourcepath .  $(find . -name "*.java")
+$JAVA9_BIN/javac -d . --module-path ../jars --module-source-path .  $(find . -name "*.java")
