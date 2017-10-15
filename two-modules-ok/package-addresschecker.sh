33371631353b301ab4328a5b2f160d5592acf7dd
Mehul Garnara <mehul.garnara@capitalone.com>
2017-10-15 17:00:52 -0400

    chore(build): create bin directory before creating jar

--- two-modules-ok/package-addresschecker.sh
+++ two-modules-ok/package-addresschecker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -x #echo on
+mkdir bin
 $JAVA9_BIN/jar --create --file=bin/addresschecker.jar --module-version=1.0 --main-class=de.codecentric.addresschecker.api.Run -C de.codecentric.addresschecker .
--- two-modules-ok/package-zipvalidator.sh
+++ two-modules-ok/package-zipvalidator.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -x #echo on
+mkdir bin
 $JAVA9_BIN/jar --create --file=bin/zipvalidator.jar --module-version=1.0 -C de.codecentric.zipvalidator .
